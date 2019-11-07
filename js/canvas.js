9d80212256466fb70d696fc1be81754629ac3772
Angela <angelabair52@gmail.com>
2019-11-07 18:08:25 -0500

    moved files to correct folder

--- js/canvas.js
+++ js/canvas.js
@@ -79,3 +79,26 @@ function updateSakura() {
 
 setInterval(updateSakura, 50);
 sakura();
+
+(function ($) {
+  $(document).ready(function(){
+    
+	// hide .navbar first
+	$(".navbar").hide();
+	
+	// fade in .navbar
+	$(function () {
+		$(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+			if ($(this).scrollTop() > 100) {
+				$('.navbar').fadeIn();
+			} else {
+				$('.navbar').fadeOut();
+			}
+		});
+
+	
+	});
+
+});
+  }(jQuery));
