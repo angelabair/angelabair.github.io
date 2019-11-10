d00bf5d697f0a35df6b45c7841932091731693fa
Angela <angelabair52@gmail.com>
2019-11-10 03:03:33 -0500

    Added sticky navbar function

--- js/canvas.js
+++ js/canvas.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("canvas");
 var c = canvas.getContext("2d");
-var particlesOnScreen = 300;
+var particlesOnScreen = 250;
 var particlesArray = [];
 var w,h;
 w = canvas.width = window.innerWidth;
@@ -23,9 +23,9 @@ function sakura() {
 			x: Math.random() * w,
 			y: Math.random() * h,
 			opacity: Math.random(),
-			speedX: random(-11, 11),
-			speedY: random(7, 15),
-			radius: random(0.5, 4.2)
+			speedX: random(-11, 1),
+			speedY: random(7, 8),
+			radius: random(1,4.5)
 		})
 	}
 };
@@ -41,18 +41,24 @@ function drawSakura() {
 			particlesArray[i].radius
 			);
 
-		gradient.addColorStop(0, "rgba(255, 255, 255)"); //white
-		gradient.addColorStop(1, "rgba(255, 183, 197)"); //pink
+		gradient.addColorStop(0, "rgba(255, 255, 255," + particlesArray[i].opacity + ")");  // white
+        gradient.addColorStop(1, "rgba(255, 183, 197," + particlesArray[i].opacity + ")");   // pink
+
 
 		c.beginPath();
+		// c.save();
+		// c.translate(particlesArray[i].x, particlesArray[i].y);
 		c.arc(
 			particlesArray[i].x,
 			particlesArray[i].y,
 			particlesArray[i].radius,
+			// particlesArray[i].radius * 2,
+			// Math.PI / 4,
 			0,
-			Math.PI * 2,
+			Math.PI *2,
 			false
 			);
+
 
 		c.fillStyle = gradient;
 		c.fill();
@@ -65,7 +71,7 @@ function fallingSakura() {
 		particlesArray[i].y += particlesArray[i].speedY;
 
 		if (particlesArray[i].y > h) {
-			particlesArray[i].x = Math.random() * w * 1.5;
+			particlesArray[i].x = Math.random() * w * 1.5
 			particlesArray[i].y = -50;
 		}
 	}
@@ -80,25 +86,25 @@ function updateSakura() {
 setInterval(updateSakura, 50);
 sakura();
 
-(function ($) {
-  $(document).ready(function(){
+// (function ($) {
+//   $(document).ready(function(){
     
-	// hide .navbar first
-	$(".navbar").hide();
+// 	// hide .navbar first
+// 	$(".navbar").hide();
 	
-	// fade in .navbar
-	$(function () {
-		$(window).scroll(function () {
-            // set distance user needs to scroll before we fadeIn navbar
-			if ($(this).scrollTop() > 100) {
-				$('.navbar').fadeIn();
-			} else {
-				$('.navbar').fadeOut();
-			}
-		});
+// 	// fade in .navbar
+// 	$(function () {
+// 		$(window).scroll(function () {
+//             // set distance user needs to scroll before we fadeIn navbar
+// 			if ($(this).scrollTop() > 100) {
+// 				$('.navbar').fadeIn();
+// 			} else {
+// 				$('.navbar').fadeOut();
+// 			}
+// 		});
 
 	
-	});
+// 	});
 
-});
-  }(jQuery));
+// });
+//   }(jQuery));
